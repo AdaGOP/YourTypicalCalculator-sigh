0caa44de575aa46f29f945cb9b59b028d78e402e
Allicia Viona <alliciawn@gmail.com>
2022-09-14 11:26:05 +0700

    Initial commit -- adding main calculator function (add, multiply, divide by)

---
+++ YourTypicalCalculator/Formulas.swift
@@ -0,0 +1,24 @@
+//
+//  Formulas.swift
+//  YourTypicalCalculator
+//
+//  Created by Allicia Viona Sagi on 14/09/22.
+//
+
+import Foundation
+
+class Formulas {
+    
+    func addNumbers(x: Int, y: Int) -> Int {
+        return x + y
+    }
+    
+    func multiplyNumbers(x: Int, y: Int) -> Int {
+        return x * y
+    }
+    
+    func divideNumbers(x: Int, y: Int) -> Int {
+        return x / y
+    }
+    
+}
