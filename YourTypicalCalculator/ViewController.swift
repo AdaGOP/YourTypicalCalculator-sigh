6649c9d352ffad44fc9547af890ad48cbaa1a389
Allicia Viona <alliciawn@gmail.com>
2022-09-14 11:13:42 +0700

    Initial Commit

---
+++ YourTypicalCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YourTypicalCalculator
+//
+//  Created by Allicia Viona Sagi on 14/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
